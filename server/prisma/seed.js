0c379ef6110028a3d98fd2bafb09f3b43aacf3f7
haisyaf <haidar.syaafie@gmail.com>
2026-05-23 22:43:41 +0700

    fix: update subscription plan IDs in seed script

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -3,7 +3,7 @@ const prisma = require("../lib/prisma");
 async function main() {
   const plans = [
     {
-      id: "11111111-1111-1111-1111-111111111111",
+      id: "c0de0000-0000-4000-8000-000000000001",
       name: "Basic",
       price: 0,
       features: {
@@ -13,7 +13,7 @@ async function main() {
       },
     },
     {
-      id: "22222222-2222-2222-2222-222222222222",
+      id: "c0de0000-0000-4000-8000-000000000002",
       name: "Pro",
       price: 99000,
       features: {
